04e038b5c7de459767718740ed1d3808cef3a254
pengzhanbo <volodymyr@foxmail.com>
2023-04-04 00:27:10 +0800

    feat: add code line numbers

--- electron/main/markdown.ts
+++ electron/main/markdown.ts
@@ -25,7 +25,12 @@ export async function setupMarkdown() {
   mdi.options.highlight = (code, lang) => {
     const shortLang = codeLanguageAlias[lang] || lang
     const content = highlighter.codeToHtml(code, { lang })
-    return `<div class="language-${lang}" data-lang="${shortLang}"><span class="copy-code"></span>${content}</div>`
+    const lines = content.split('\n').slice(0, -1)
+    const lineNumbersCode = lines
+      .map(() => '<div class="line-number"></div>')
+      .join('')
+
+    return `<div class="language-${lang}" data-lang="${shortLang}"><span class="copy-code"></span>${content}<div class="line-numbers" aria-hidden="true">${lineNumbersCode}</div></div>`
   }
 }
 
